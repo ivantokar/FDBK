9eacd6d5975d130242a718044e22132abec05731
Ivan Tokar <i.j.tokar@gmail.com>
2017-12-12 22:16:15 +0200

    validation

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -9,43 +9,45 @@ let router = express.Router();
 router.post('/', (request, response) => {
 	const { errors, isValid } = validateInput(request.body);
 
-	if (isValid) {
-		if (process.env.MESSAGE_SENDING === 'yes') {
-			let transporter = nodemailer.createTransport({
-				service: process.env.MAIL_SERVICE,
-				auth: {
-					user: process.env.MAIL_USER,
-					pass: process.env.MAIL_PASSWORD
-				}
-			});
-
-			let options = {
-				from: process.env.MAIL_FROM,
-				to: process.env.MAIL_TO,
-				subject: process.env.MAIL_SUBJECT,
-				text: request.body.email +'\n\n'+ request.body.message
-			};
-
-			transporter.sendMail(options, (error) => {
-				if (error) {
-					return console.log(error);
-				}
-			});
-		}
-
-		if (process.env.MESSAGE_STORING === 'yes') {
-			sequelize.sync().then(() => {
-				return Message.create({
-					email: request.body.email,
-					message: request.body.message,
-				});
-			});
-		}
-
-		response.status(200).json({ success: true, message: 'Thank you for contacting us. We will get back to you soon!' });
-	} else {
+	if (!isValid) {
 		response.status(403).json(errors);
+		return;
 	}
+
+	if (process.env.MESSAGE_SENDING === 'yes') {
+		let transporter = nodemailer.createTransport({
+			service: process.env.MAIL_SERVICE,
+			auth: {
+				user: process.env.MAIL_USER,
+				pass: process.env.MAIL_PASSWORD
+			}
+		});
+
+		let options = {
+			from: process.env.MAIL_FROM,
+			to: process.env.MAIL_TO,
+			subject: process.env.MAIL_SUBJECT,
+			text: request.body.email +'\n\n'+ request.body.message
+		};
+
+		transporter.sendMail(options, (error) => {
+			if (error) {
+				return console.log(error);
+			}
+		});
+	}
+
+	if (process.env.MESSAGE_STORING === 'yes') {
+		sequelize.sync().then(() => {
+			return Message.create({
+				email: request.body.email,
+				message: request.body.message,
+			});
+		});
+	}
+
+	response.status(200).json({ success: true, message: 'Thank you for contacting us. We will get back to you soon!' });
+
 });
 
 module.exports = router;
